81cb2d656c717008acc83330c85a80afdf13b486
GioRuizMZ <ruizmezagiovanni@gmail.com>
2025-07-18 13:33:33 -0600

    historial y errores solucionados

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from src.ui.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+    with open("styles.qss", "r") as f:
+        qss = f.read()
+        print("QSS cargado:", qss[:100])  # Muestra los primeros 100 caracteres
+        app.setStyleSheet(qss)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
